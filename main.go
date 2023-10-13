bf2a2ad0a88fa62e37903671a058f7afebb0102e
root9464 <senpai9464@yandex.ru>
2023-10-13 17:51:40 +0500

    13.10.2023

--- main.go
+++ main.go
@@ -1,44 +1,35 @@
+// Сортировка вставками (Insertion sort) + метод рандомного массива
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+)
 
-type user struct {
-	id      int16
-	name    string
-	surname string
+func insertionSorted(array []int) []int {
+	length := len(array)
+
+	for i := 1; i < length; i++ {
+		j := i //переопределние для обнуления счетчика
+		for j > 0 {
+			if array[j-1] > array[j] {
+				array[j-1], array[j] = array[j], array[j-1]
+
+			}
+			j = j - 1
+		}
+
+	}
+	return array
 }
 
-func debug(u user) {
-	fmt.Printf("%+v\n", u)
-}
-
-// notify реализует метод вывода.
-func (u user) notify() {
-	fmt.Println("Система: добавлен", u.name, u.surname)
-}
-
-// реализация геттер свойств  .
-func (u *user) changeSurname(surname string) {
-	u.surname = surname
-}
-
-func (u *user) changeName(name string) {
-	u.name = name
-}
-
-func (u *user) Send() {
-	u.notify()
-}
-
-// main - это точка входа для приложения.
 func main() {
-	// Указатели типа user также могут использоваться для методов
-	// объявлен с получателем значения.
+	len := int(5) // обьявление числа которое будет будущей длиной рандомного (неотсортированного) массива
+	// создание рандомного (неотсортированного) массива
+	array := rand.Perm(len)
+	fmt.Println(array)
 
-	user1 := &user{1, "Egor", "Chervony"}
-	user1.changeName("Егор")
-	user1.changeSurname("Червоный")
-	user1.notify()
-	debug(user{1, "Egor", "Chervony"})
-	fmt.Println("или же" + "{id:1 name:Egor surname:Chervony}") // не нашел ftm свойство чтобы читать много обьектов
+	// сортировка неотсортированного массива
+	sort := insertionSorted(array)
+	fmt.Println(sort)
 }
